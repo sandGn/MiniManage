e3a2d96137695f935d217f5df37bdf6b91e6cac0
HOME-SAND\Sand <sand.mail@foxmail.com>
2019-12-23 22:15:00 +0800

    userlayout router config

--- config/router.config.js
+++ config/router.config.js
@@ -1,20 +1,62 @@
-/**
+import { RouteView } from '../src/layouts'
+export const asyncRouterMap = [
+  {
+    path: '/',
+    name: 'index',
+    component: BasicLayout,
+    meta: { title: '首页' },
+    redirect: '/dashboard/workplace',
+    children: [
+      {
+        //仪表盘
+        path: '/dashboard',
+        name: 'dashboard',
+        redirect: '/dashboard/workplace',
+        component: RouteView,
+        meta: { title: '仪表盘', keepAlive: true, icon: 'dashboard', permission: ['dashboard'] },
+        children: [
+          {
+            //工作台
+            path: '/dashboard/workplace',
+            name: 'Workplace',
+            component: () => import('@/views/dashboard/Workplace'),
+            meta: { title: '工作台', keepAlive: true, permission: ['dashboard'] }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '*', redirect: '/404', hidden: true
+  }
+]
+
+/*
  * 基础路由
- * 
  */
-import Home from '@/views/Home.vue'
 export const constantRouterMap = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/views/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
-    }
+  {
+    path: '/user',
+    component: UserLayout,
+    redirect: '/user/login',
+    hidden: true,
+    children: [
+      {
+        //登录
+        path: 'login',
+        name: 'login',
+        component: () => import('@/views/user/Login')
+      },
+      {
+        path: 'register',
+        name: 'register',
+        component: () => import('@/views/user/Register')
+      },
+      {
+        path: 'register-result',
+        name: 'registerResult',
+        component: () => import('@/views/user/RegisterResult')
+      }
+    ]
+  }
 ]
---
+++ src/permission.js
@@ -0,0 +1,16 @@
+/*
+ * 全局导航守卫
+ */
+import Vue from 'vue'
+import router from './router'
+import store from './store'
+
+// to: Route: 即将要进入的目标 路由对象
+// from: Route: 当前导航正要离开的路由
+router.beforeEach((to, from, next) => {
+
+})
+
+router.afterEach(() => {
+
+})
